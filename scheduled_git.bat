a8fc86338bc564ca1d9ac08534e3d25f089df3b7
Erik Holum <eholum@gmail.com>
2024-06-06 22:11:41 -0400

    Fix script

--- scheduled_git.bat
+++ scheduled_git.bat
@@ -1,4 +1,3 @@
 @echo off
-"C:\Program Files\Git\bin\bash.exe" \
-	-c "bash /c/Users/eholu_u9uwjhd/Documents/windows_schedule/git_commit_push.sh /c/Windows/Temp/test" 
+"C:\Program Files\Git\bin\bash.exe" -c "bash /c/Users/eholu_u9uwjhd/Documents/windows_schedule/git_commit_push.sh /c/Windows/Temp/test"
 
